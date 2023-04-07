07fcc6cc9cac5158d27214b1b7ea0fcfcb388833
byron <bgguerra@scl.edu.gt>
2023-04-07 13:50:13 -0600

    prueba uno

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function desplegarMenu (){
+    let btnMenu = document.querySelector(".btn-menu")
+    let menu = document.querySelector(".menu")
+
+    btnMenu.addEventListener("click", function (){
+        menu.classList.toggle("abierto")
+    })
+}
+
+desplegarMenu();
